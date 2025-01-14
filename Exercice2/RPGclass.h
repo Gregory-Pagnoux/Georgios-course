35fd6be29e2f2edf8c9454af1f6550dae89da3b4
Grégory <gregory.pagnoux@algosup.com>
2025-01-14 16:35:32 +0100

    update

--- Exercice2/RPGclass.h
+++ Exercice2/RPGclass.h
@@ -1,4 +1,9 @@
+#include <iostream>
+#include <string>
+
 #include "balanceSTB.h"
+
+using namespace std;
 
 class RpgClass {
     public:
@@ -6,23 +11,12 @@ class RpgClass {
         RpgClass() : name(""), playerClass(""), weaponType(""){}
         RpgClass(string n, string x, string y) : name(n), playerClass(x), weaponType(y){}
 
-        string getName() const (return name);
-        string getPlayerClass() const (return playerClass);
-        string getWeaponType() const (return weaponType);
+        //getters
+        string getName() const {return name;}
+        string getPlayerClass() const {return playerClass;}
+        string getWeaponType() const {return weaponType;}
     private:
         string name;
         string playerClass;
         string weaponType;
 };
-
-void getName(){
-    
-}
-
-void getPlayerClass(){
-
-}
-
-void getWeaponType(){
-
-}
--- Exercice2/main.cpp
+++ Exercice2/main.cpp
@@ -1,4 +1,9 @@
+#include <iostream>
+#include <string>
+
 #include "RPGclass.h"
+
+using namespace std;
 
 int main(){
     int array[5] = {27, 30, 45, 67, 69};
@@ -6,5 +11,13 @@ int main(){
         insert(array[i]);
     }
     
+    RpgClass Character1("Arthas", "Paladin", "Hammer");
+    RpgClass Character2("Aro", "Dwarf", "Knife");
+    RpgClass Character3("Beatis", "Sorcer", "Wand");
+    RpgClass Character4("Zanita", "Dwarf", "Sword");
+
+    RpgClass CharacterName = Character1.getName();
+    cout << "name: " << CharacterName << endl;
+
     return 0;
 }
