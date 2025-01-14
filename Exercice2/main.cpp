827f9285d4cfd6d683e101d3dd557e8b2c6ef87d
Grégory <gregory.pagnoux@algosup.com>
2025-01-14 16:41:15 +0100

    update

--- Exercice2/main.cpp
+++ Exercice2/main.cpp
@@ -6,18 +6,19 @@
 using namespace std;
 
 int main(){
-    int array[5] = {27, 30, 45, 67, 69};
-    for (int i = 0; i < 7; i++){
-        insert(array[i]);
-    }
-    
     RpgClass Character1("Arthas", "Paladin", "Hammer");
     RpgClass Character2("Aro", "Dwarf", "Knife");
     RpgClass Character3("Beatis", "Sorcer", "Wand");
     RpgClass Character4("Zanita", "Dwarf", "Sword");
 
-    RpgClass CharacterName = Character1.getName();
-    cout << "name: " << CharacterName << endl;
+    string CharacterName1 = Character1.getName();
+    cout << "name 1: " << CharacterName1 << endl;
+    string CharacterClass1 = Character1.getPlayerClass();
+    cout << "player class 1: " << CharacterClass1 << endl;
+    string CharacterWeapon1 = Character1.getWeaponType();
+    cout << "weapon type 1: " << CharacterWeapon1 << endl;
+    string CharacterName2 = Character2.getName();
+    cout << "name 2: " << CharacterName2 << endl;
 
     return 0;
 }
